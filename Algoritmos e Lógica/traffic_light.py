c71899fc58d18e10ed87291da6751694fa134e2c
Rafael Meireles <rafameireles2011@gmail.com>
2022-01-16 12:57:17 -0300

    Create traffic_light.py

---
+++ Algoritmos e Lógica/traffic_light.py
@@ -0,0 +1,10 @@
+"""
+Crie um código em python que atenda aos seguintes requisitos:
+1 - Pergunte ao usuário qual a cor atual do semáforo.
+2 - Verde pode prosseguir, amarelo = atenção reduza a velocidade.
+vermelho, pare.
+3 - Se for vermelho o código deverá mostrar um contador de 5 segundos.
+4 - Terminando a contagem deverá aparecer uma pergunta, deseja continuar a viagem?
+Se sim, retorna a pergunta "Qual a cor da luz do semáforo?" Se escolher não, a mensagem 
+"Dirija com cuidado, tenha um bom dia" deverá ser exibida e o programa finalizado.
+"""
